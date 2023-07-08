7f7e6739ce9cfc9903a04e3149d7388199797f8b
Mohtashim_Wajid <91398268+Mohtashim-Wajid@users.noreply.github.com>
2023-07-09 00:44:52 +0500

    review created

---
+++ src/components/Carousel.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import review from '../assets/reviews.jpeg'
+const Carousel = () => {
+    return (
+        <>
+            <div id="carouselExampleFade" className="carousel slide carousel-fade">
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img src={review} className="" alt="review"/>
+                    </div>
+                    <div className="carousel-item">
+                        <img src="..." className="d-block w-100" alt="..."/>
+                    </div>
+                    <div className="carousel-item">
+                        <img src="..." className="d-block w-100" alt="..."/>
+                    </div>
+                </div>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
+        </>
+    )
+}
+
+export default Carousel
