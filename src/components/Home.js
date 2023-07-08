75e51cde3ea4a7d9bdb83bd17b847188295956db
exprodev <k191115@nu.edu.pk>
2023-07-08 22:34:31 +0500

    XX

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,22 @@ import React from 'react';
 import './Home.css';
 
 const Home = () => {
+  const handleScrollToAbout = () => {
+    // Scroll to the 'about' section when the arrow is clicked
+    document.getElementById('about').scrollIntoView({
+      behavior: 'smooth',
+    });
+  };
+
   return (
     <section id="home" className="section home-section">
-      <h1>Welcome to our Website!</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      <a href='#about'>Next</a>
+      <div className='HomeText'>
+        <h1>WE MOVE YOU</h1>
+        <h2>WITH QUALITY CARE & SAVING!</h2>
+        <p>Over thirty years of experience with logistics.</p>
+        <button>GET A QUOTE</button>
+      </div>
+      <div class="down-arrow" onClick={handleScrollToAbout}></div>
     </section>
   );
 }
