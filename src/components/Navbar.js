d19f5d6265cde8dd0ba2ac44ff5e5fe4f5653151
Mohtashim_Wajid <91398268+Mohtashim-Wajid@users.noreply.github.com>
2023-07-08 23:17:31 +0500

    centralized nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,16 +7,17 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons';
 const Navbar = () => {
   return (
     <nav className="navbar">
-      
-      <img src={logo} alt="logo_logistics" className="image-logo"/>
-      {/* Your navigation links */}
-      <ul>
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About Us</a></li>
-        <li><a href="#services">Services</a></li>
-        <li><a href="#contact">Contact Us</a></li>
-        <li><FontAwesomeIcon icon={faSearch} className="search-icon" /></li>
-      </ul>
+      <div className="container">
+        <img src={logo} alt="logo_logistics" className="image-logo"/>
+        {/* Your navigation links */}
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About Us</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#contact">Contact Us</a></li>
+          <li><FontAwesomeIcon icon={faSearch} className="search-icon" /></li>
+        </ul>
+      </div>
     </nav>
   );
 }
