dde9c830fdbd4665357839a0269af1da2618c492
Mohtashim_Wajid <91398268+Mohtashim-Wajid@users.noreply.github.com>
2023-07-09 18:52:11 +0500

    more changes

--- src/components/End.js
+++ src/components/End.js
@@ -17,8 +17,30 @@ function End() {
             <div className='quick-links'>
 
             </div>
-            <div className='information'>Information</div>
-            <div className='subscribe'>Subscribe to our News Letter</div>
+            <div className='information'>
+                <p className="section-heading text-capitalize">
+                    information
+                </p>
+                <p className="section-content">
+                    <i class="fa-solid fa-phone-volume"></i>
+                    (123) 456-78910
+
+                </p>
+            </div>
+
+
+            <div className='subscribe p-3'>
+                <p className="section-heading text-capitalize">
+                    Subscribe to our news letter
+                </p>
+                <p className="section-content">
+                    We'll keep you posted with news and updates
+                </p>
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control button-width" placeholder="Enter your email" aria-label="Recipient's username" aria-describedby="button-addon2"/>
+                    <button class="btn btn-outline-secondary color-blue text-uppercase px-3" type="button" id="button-addon2">subscribe</button>
+                </div>
+            </div>
         </div>
     )
 }
