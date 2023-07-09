aadb1cc03a88a81268e01977c520e18c7830f6f8
exprodev <k191115@nu.edu.pk>
2023-07-09 15:57:56 +0500

    VVV

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,53 +4,57 @@ import truck from '../assets/truck.jpg'
 const Contact = () => {
   return (
     <section id="contact" className="section contact-section">
-      <div className="container centralize">
-        <div className="row w-100">
-          <div className="col-5 img-container">
-            <img src={truck} alt="truck" className='truck-img' />
-          </div>
-          <div className="col-7 form-container">
-            <div className="container">
-              <p className='contact-heading'>Contact Us</p>
-              <div className="row">
-                <div className="col">
-                  <div className="form-group">
-                    <label htmlFor="fullName">Full Name</label>
-                    <input type="text" id="fullName" className="form-control" placeholder="Enter your full name" />
+      <div className='bottom'>
+        <div className="container centralize contact-page">
+          <div className="row w-100">
+            <div className="col-5 img-container">
+              <img src={truck} alt="truck" className='truck-img' />
+            </div>
+            <div className="col-7 form-container">
+              <div className="container">
+                <p className='contact-heading'>Contact Us</p>
+                <div className="row">
+                  <div className="col">
+                    <div className="form-group">
+                      <label htmlFor="fullName">Full Name</label>
+                      <input type="text" id="fullName" className="form-control" placeholder="Enter your full name" />
+                    </div>
+                  </div>
+                  <div className="col">
+                    <div className="form-group">
+                      <label htmlFor="phoneNumber">Phone Number</label>
+                      <input type="text" id="phoneNumber" className="form-control" placeholder="Enter your phone number" />
+                    </div>
                   </div>
                 </div>
-                <div className="col">
-                  <div className="form-group">
-                    <label htmlFor="phoneNumber">Phone Number</label>
-                    <input type="text" id="phoneNumber" className="form-control" placeholder="Enter your phone number" />
+                <div className="row">
+                  <div className="col">
+                    <div className="form-group">
+                      <label htmlFor="email">Email</label>
+                      <input type="text" id="email" className="form-control" placeholder="Enter your email" />
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="row">
-                <div className="col">
-                  <div className="form-group">
-                    <label htmlFor="email">Email</label>
-                    <input type="text" id="email" className="form-control" placeholder="Enter your email" />
+                <div className="row">
+                  <div className="col">
+                    <div className="form-group">
+                      <label htmlFor="message">Message</label>
+                      <textarea id="message" className="form-control" placeholder="Enter your message"></textarea>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="row">
-                <div className="col">
-                  <div className="form-group">
-                    <label htmlFor="message">Message</label>
-                    <textarea id="message" className="form-control" placeholder="Enter your message"></textarea>
+                <div className="row">
+                  <div className="col">
+                    <button className="btn submit text-uppercase py-2 w-100 my-4" type='submit'>
+                      Submit
+                    </button>
                   </div>
-                </div>
-              </div>
-              <div className="row">
-                <div className="col">
-                  <button className="btn submit text-uppercase py-2 w-100 my-4" type='submit'>
-                    Submit
-                  </button>
                 </div>
               </div>
             </div>
           </div>
+        </div>
+        <div className='background'>
         </div>
       </div>
     </section>
